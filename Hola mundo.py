3893aa2396ad0cb1def2bffab401f9e9d889cb0e
NightWatcher <andoni.hilario.lazaro@gmail.com>
2024-09-18 11:56:46 +0200

    Aprendiendo a usar type en python

--- Hola mundo.py
+++ Hola mundo.py
@@ -1,1 +1,12 @@
 print("Hola, mundo!")
+
+# PRUEBAS CON LOS TIPOS DE DATOS
+
+x = int(input("Dame un numero: "))
+
+print(type(x))
+
+if type(x) == int:
+    print("La variable X es un entero")
+else:
+    print("No se que tipo de variable es X")
