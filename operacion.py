f5b143c53b021221b770fc0e735b5d1a896660a9
NightWatcher <andoni.hilario.lazaro@gmail.com>
2024-09-18 13:03:02 +0200

    Archivo de operaciones comentado y actualizado

--- operacion.py
+++ operacion.py
@@ -5,21 +5,26 @@ n2= int(input("Dame un numero: "))
 
 operacion = input("¿Que operacion quieres realizar? ")
 
-# VAMOS A HACER UNA SUMA
+# SELECCION DE LA OPERACION QUE QUEREMOS REALIZAR
 suma = n1 + n2
 while operacion.upper() != "Q":
+    # REALIZAR UNA SUMA
     if operacion.upper() == "SUMA" or operacion.upper() == "SUMAR":
         suma = n1 + n2
         print(f"De acuerdo, el resultado de la suma es: {suma}")
+    # REALIZAR UNA RESTA
     elif operacion.upper() == "RESTA" or operacion.upper() == "RESTAR":
         resta = n1 - n2
         print(f"De acuerdo, el resultado de la resta es: {resta}")
+    # REALIZAR UNA DIVISION
     elif operacion.upper() == "DIVISION":
         division = n1 / n2
         print(f"De acuerdo, el resultado de la division es: {division}")
+    # REALIZAR UNA MULTIPLICACION
     elif operacion.upper() == "MULTIPLICACION" or operacion.upper() == "MULTIPLICAR":
         multiplicar = n1 * n2
         print(f"De acuerdo, el resultado de la multiplicacion es: {multiplicar}")
+    # NO REALIZAR NADA Y SALIR DEL PROGRAMA
     elif operacion.upper() == "Q":
         break
     
