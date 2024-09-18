61e86d9afe02b070ba98dda5a5edb1ff658ad76f
NightWatcher <andoni.hilario.lazaro@gmail.com>
2024-09-18 14:43:52 +0200

    Incluido nuevo tipo de datos en el archivo tipo de datos

--- Introduccion a la programacion/Introduccion a python/tipos de datos.py
+++ Introduccion a la programacion/Introduccion a python/tipos de datos.py
@@ -1,10 +1,16 @@
 # PRUEBAS CON LOS TIPOS DE DATOS
 
 x = int(input("Dame un numero: "))
+y = input("Dame una palabra o una frase: ")
 
-print(type(x))
+print(type(y))
 
 if type(x) == int:
     print("La variable X es un entero")
 else:
     print("No se que tipo de variable es X")
+
+if type(y) == str:
+    print("La variable y es una string")
+else:
+    print("No se que tipo de variable es X")    
