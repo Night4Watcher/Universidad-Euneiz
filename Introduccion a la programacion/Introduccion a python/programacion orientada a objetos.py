61d899bc7c2b7d0f358fd6557fb3ec0ccb133cbd
NightWatcher <andoni.hilario.lazaro@gmail.com>
2024-09-18 17:01:18 +0200

    Programa orientacion a objetos finalizado

--- Introduccion a la programacion/Introduccion a python/programacion orientada a objetos.py
+++ Introduccion a la programacion/Introduccion a python/programacion orientada a objetos.py
@@ -1,3 +1,4 @@
+# OBJETO LLAMADO ANIMAL CREADO Y SUS POSIBLES CARACTERISTICAS Y "CAPACIDADES"
 class Animal:
     def __init__(self, tipo_animal, nombre, raza):
         self.tipo_animal = tipo_animal
@@ -15,13 +16,15 @@ class Animal:
         elif self.tipo_animal == "gato":
             print(f"{self.nombre}, que es un {self.raza} ha decidido: maullar")
             
+# DEFINICION DE LA CARACTERISTICA DE TIPO DE ANIMAL POR EL USUARIO EN FUNCION A LA LISTA DE ANIMALES POSIBLES
 lista_animales_posibles = ["GATO", "PERRO"]
 mi_animal = input("¿Qué tipo de animal tienes en casa? ")
 
 while mi_animal.upper() not in lista_animales_posibles:
     print("Lo sentimos, este programa unicamente esta disponible para perros y gatos")
     mi_animal = input("¿Qué tiene un perro o un gato? ")
-    
+
+# DEFINICION DE LA CARACTERISTICA DE RAZA DEL ANIMAL EN FUNCION AL TIPO DE ANIMAL Y LA LISTA DE RAZAS DISPONIBLES POR EL USUARIO
 razas_perros = ["LABRADOR", "BORDER COLLIE", "BORDERCOLLIE", "BULLDOG", "PASTOR ALEMAN", "PASTORALEMAN"]
 razas_gatos = ["RAGDOLL", "GATO PERSA", "BURMÉS", "SCOTTISH FOLD"]
 raza_animal = input("¿Cuál es la raza de tu animal? ")
@@ -35,6 +38,7 @@ elif mi_animal.upper() == "GATO":
         print("Lo sentimos, esa no es una raza de gatos que tengamos disponible.")
         raza_animal = input("Por favor, pruebe de nuevo: ")
         
+# DEFINICION DE LA CARACTERISTICA DEL NOMBRE DEL ANIMAL POR EL USUARIO
 nombre_animal = input("¿Cuál es el nombre de tu animal?")
 
 animal_terminado = Animal(mi_animal.capitalize(), nombre_animal.capitalize(), raza_animal.capitalize())
