b6b7918e955e1ff59581969595355dcf32a3133d
NightWatcher <andoni.hilario.lazaro@gmail.com>
2024-09-18 12:07:21 +0200

    modificacion del archivo

--- Hola mundo.py
+++ Hola mundo.py
@@ -1,12 +1,3 @@
+#IMPRIMIR HOLA MUNDO EN PANTALLA
+
 print("Hola, mundo!")
-
-# PRUEBAS CON LOS TIPOS DE DATOS
-
-x = int(input("Dame un numero: "))
-
-print(type(x))
-
-if type(x) == int:
-    print("La variable X es un entero")
-else:
-    print("No se que tipo de variable es X")
