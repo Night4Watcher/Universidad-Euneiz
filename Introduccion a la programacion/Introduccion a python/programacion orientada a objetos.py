fcbc64988ecb2d1fd4dd3d49b035cd2047d23f18
NightWatcher <andoni.hilario.lazaro@gmail.com>
2024-09-18 16:55:51 +0200

    Programacion orientada a objetos terminada

--- Introduccion a la programacion/Introduccion a python/programacion orientada a objetos.py
+++ Introduccion a la programacion/Introduccion a python/programacion orientada a objetos.py
@@ -4,16 +4,39 @@ class Animal:
         self.nombre = nombre
         self.raza = raza
     
+    def nombramiento(self):
+        print(f"Mi animal es un: {self.tipo_animal}\n"
+              f"Su nombre es: {self.nombre}\n"
+              f"Su raza es: {self.raza}")
+    
     def ruido(self):
         if self.tipo_animal == "perro":
             print(f"{self.nombre}, que es un {self.raza} ha decidido: Ladrar")
         elif self.tipo_animal == "gato":
             print(f"{self.nombre}, que es un {self.raza} ha decidido: maullar")
             
-lista_animales_posibles = ["GATO, PERRO"]
+lista_animales_posibles = ["GATO", "PERRO"]
 mi_animal = input("¿Qué tipo de animal tienes en casa? ")
 
 while mi_animal.upper() not in lista_animales_posibles:
     print("Lo sentimos, este programa unicamente esta disponible para perros y gatos")
-    mi_animal = input("¿Qué tiene un perro o un gato?")
+    mi_animal = input("¿Qué tiene un perro o un gato? ")
     
+razas_perros = ["LABRADOR", "BORDER COLLIE", "BORDERCOLLIE", "BULLDOG", "PASTOR ALEMAN", "PASTORALEMAN"]
+razas_gatos = ["RAGDOLL", "GATO PERSA", "BURMÉS", "SCOTTISH FOLD"]
+raza_animal = input("¿Cuál es la raza de tu animal? ")
+
+if mi_animal.upper() == "PERRO":
+    while raza_animal.upper() not in razas_perros:
+        print("Lo sentimos, esa no es una raza de perros que tengamos disponibles.")
+        raza_animal = input("Por favor, pruebe de nuevo: ")
+elif mi_animal.upper() == "GATO":
+    while raza_animal.upper() not in razas_gatos:
+        print("Lo sentimos, esa no es una raza de gatos que tengamos disponible.")
+        raza_animal = input("Por favor, pruebe de nuevo: ")
+        
+nombre_animal = input("¿Cuál es el nombre de tu animal?")
+
+animal_terminado = Animal(mi_animal.capitalize(), nombre_animal.capitalize(), raza_animal.capitalize())
+
+animal_terminado.nombramiento()
