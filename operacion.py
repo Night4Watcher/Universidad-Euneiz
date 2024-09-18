6293ab466264ea58e14b72285b4770b2f6a47b73
NightWatcher <andoni.hilario.lazaro@gmail.com>
2024-09-18 12:55:56 +0200

    Operadores logicos

--- operacion.py
+++ operacion.py
@@ -11,24 +11,26 @@ while operacion.upper() != "Q":
     if operacion.upper() == "SUMA" or operacion.upper() == "SUMAR":
         suma = n1 + n2
         print(f"De acuerdo, el resultado de la suma es: {suma}")
-    elif operacion.upper == "RESTA" or operacion.upper == "RESTAR":
+    elif operacion.upper() == "RESTA" or operacion.upper() == "RESTAR":
         resta = n1 - n2
         print(f"De acuerdo, el resultado de la resta es: {resta}")
-    elif operacion.upper == "DIVISION":
+    elif operacion.upper() == "DIVISION":
         division = n1 / n2
         print(f"De acuerdo, el resultado de la division es: {division}")
-    elif operacion.upper == "MULTIPLICACION" or operacion.upper == "MULTIPLICAR":
+    elif operacion.upper() == "MULTIPLICACION" or operacion.upper() == "MULTIPLICAR":
         multiplicar = n1 * n2
         print(f"De acuerdo, el resultado de la multiplicacion es: {multiplicar}")
+    elif operacion.upper() == "Q":
+        break
     
     operacion = input("¿Que operacion quieres realizar? ")
-    if operacion.upper != "Q":
+    if operacion.upper() != "Q":
         cambio_numeros = input("¿Quieres cambiar los numeros con los que hacer las operaciones? ")
-        if cambio_numeros.upper == "SI":
+        if cambio_numeros.upper() == "SI":
             n1 = int(input("Dame un numero: "))
             n2= int(input("Dame un numero: "))
-        elif cambio_numeros.upper == "NO":
+        elif cambio_numeros.upper() == "NO":
             print("De acuerdo, utilizaremos los mismos numeros")
             input("Pulsa enter para continuar...")
-    elif operacion.upper == "Q":
+    elif operacion.upper() == "Q":
         break
