109398e89f61112b46c71fe9a8876469bef2ec8e
NightWatcher <andoni.hilario.lazaro@gmail.com>
2024-09-23 10:19:03 +0200

    actualizacion

--- Introduccion a la programacion/Introduccion a python/id.py
+++ Introduccion a la programacion/Introduccion a python/id.py
@@ -3,3 +3,11 @@
 x = 3
 
 print(id(x))
+
+# PYTHON PUEDE DAR LA MISMA DIRECCION EN LA MEMORIA A DOS VARIABLES
+
+v = 5
+y = 5
+
+print(f"La direccion de memoria de la variable v es: {v}")
+print(f"La direccion de memoria de la variable y es: {y}")
