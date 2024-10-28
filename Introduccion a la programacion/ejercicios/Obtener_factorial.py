a789adf10c5e5667e20875e235ec14c57c8852cf
NightWatcher <andoni.hilario.lazaro@gmail.com>
2024-10-28 10:39:00 +0100

    Ejercicio que te entrega el factorial del numero que introduzcas

---
+++ Introduccion a la programacion/ejercicios/Obtener_factorial.py
@@ -0,0 +1,16 @@
+def factorial(numero):
+    numero = numero + 1
+    resultado = 1
+    for n in range(numero):
+        if n != 0:
+            resultado = resultado * n
+    return resultado
+
+def main():
+    numero = int(input("Dame un numero del que hacer el numero factorial: "))
+    while numero < 0:
+        print("El numero no puede ser negativo")
+        numero = int(input("Dame un numero del que hacer el numero factorial: "))
+    print(f"El factorial de {numero} es {factorial(numero)}")
+
+main()
