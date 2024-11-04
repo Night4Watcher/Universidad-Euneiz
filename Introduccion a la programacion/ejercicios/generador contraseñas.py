ef5e91867de4716ac795de7fa52ac4880f1b7936
NightWatcher <andoni.hilario.lazaro@gmail.com>
2024-11-04 11:19:21 +0100

    Programa capaz de generar contraseñas

---
+++ Introduccion a la programacion/ejercicios/generador contraseñas.py
@@ -0,0 +1,77 @@
+import random
+import string
+
+ABECEDARIO_MINUSCULA = string.ascii_lowercase
+ABECEDARIO_MAYUSCULA = string.ascii_uppercase
+NUMEROS = string.digits
+
+def comprobacion_cantidad(cantidad):
+    cantidad = str(cantidad)
+    # COMPROBACION DE QUE LA CONTRASEÑA SEA DE VALOR NUMERICO
+    while cantidad.isnumeric() == False:
+        print("La contraseña tiene que ser numerica, no vale que contenga texto.")
+        cantidad = input("Dame un numero de contraseñas que quieras crear: ")
+    # COMPROBACION DE QUE LA CONTRASEÑA SEA MAYOR DE VALOR POSITIVO
+    cantidad = int(cantidad)
+    while cantidad < 0:
+        print("La cantidad de contraseñas no puede tener un valor negativo.")
+        cantidad = int(input("Dame un numero de contraseñas que quieras crear: "))
+    # DEVOLUCION DE LA CONTRASEÑA CUMPLIENDO CON LO QUE NECESITA TENER
+    return cantidad
+
+def generador_contraseña():
+    # VARIABLES RELACIONADAS CON EL GENERADOR DE CONTRASEÑAS
+    contraseña = ""
+    cant_mayusculas = 0
+    cant_minusculas = 0
+    cant_numeros = 0
+    cant_caracteres = cant_minusculas + cant_mayusculas + cant_numeros
+    # BUCLE QUE COMPRUEBA QUE LA CONTRASEÑA QUE SE VAYA A DEVOLVER CUMPLA CON UNAS CARACTERISTICAS
+    while cant_caracteres < 12 or cant_numeros < 1 or cant_mayusculas < 1:
+        # RESETEO DE LAS VARIABLES PARA QUE NO SE ACUMULEN LOS DISTINTOS INTENTOS ALMACENADOS
+        cant_mayusculas = 0
+        cant_minusculas = 0
+        cant_numeros = 0
+        # CREACION DEL CARACTER A AÑADIR DE MANERA ALEATORIA
+        char_create = random.randint(0, 2)
+        # CREACION DEL CARACTER EN MINUSCULA
+        if char_create == 0:
+            min_create = random.choice(ABECEDARIO_MINUSCULA)
+            contraseña = contraseña + min_create
+        # CREACION DEL CARACTER EN MAYUSCULA
+        elif char_create == 1:
+            mayus_create = random.choice(ABECEDARIO_MAYUSCULA)
+            contraseña = contraseña + mayus_create
+        # CREACION DEL CARACTER NUMERICO
+        elif char_create == 2:
+            num_create = random.choice(NUMEROS)
+            contraseña = contraseña + num_create
+        for character in contraseña:
+            if character.isdigit():
+                cant_numeros += 1
+            elif character in ABECEDARIO_MAYUSCULA:
+                cant_mayusculas += 1
+            elif character in ABECEDARIO_MINUSCULA:
+                cant_minusculas += 1
+        cant_caracteres = cant_minusculas + cant_mayusculas + cant_numeros
+    # DEVOLVER LA CONTRASEÑA QUE CUMPLE CON LOS REQUISITOS SOLICITADOS
+    return contraseña
+
+def main():
+    cantidad_contraseñas = 0
+    contraseñas = []
+    # BIENVENIDA DEL USUARIO AL PROGRAMA
+    print("Bienvenido a tu generador de contraseñas personal.")
+    input("Pulsa enter para continuar...")
+    # PREGUNTAR AL USUARIO CUANTAS CONTRASEÑAS QUIERE CREAR
+    cantidad_contraseñas = input("¿Cuantas contraseñas quieres que te genere el programa? ")
+    # EJECUTAMOS LA FUNCION PARA COMPROBAR QUE EL DATO INTRODUCIDO POR EL USUARIO CUMPLE CON LO NECESARIO
+    cantidad_contraseñas = comprobacion_cantidad(cantidad_contraseñas)
+    # BUCLE DEL PROGAMA HASTA CREAR TODAS LAS CONTRASEÑAS QUE QUIERE EL USUARIO
+    while cantidad_contraseñas > 0:
+        contraseñas.append(generador_contraseña())
+        cantidad_contraseñas -= 1
+    print(f"Las contraseñas generadas han sido: {contraseñas}")
+    
+
+main()
