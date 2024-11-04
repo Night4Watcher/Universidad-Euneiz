ec8ef8d5a84d4d4737b480207387f08aa2148a8a
NightWatcher <andoni.hilario.lazaro@gmail.com>
2024-11-04 16:41:09 +0100

    Optimizacion de la funcion comprobante del valor

--- Introduccion a la programacion/ejercicios/comprobador_colecciones.py
+++ Introduccion a la programacion/ejercicios/comprobador_colecciones.py
@@ -1,10 +1,7 @@
 coleccion = [1, 4, 5, 9]
 
 def comprobador(coleccion, valor):
-    if valor in coleccion:
-        return True
-    else:
-        return False
+    return valor in coleccion
 
 def main():
     valor_comprobar = int(input("Dame un valor para comprobarlo en la coleccion: "))
